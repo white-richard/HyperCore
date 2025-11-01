fd0d05ebd2d4eed7cbfad8cc15ca4fcde3dcc393
white-richard <rich.white285@yahoo.com>
2025-11-01 14:59:13 -0700

    GRetriever caused dependency issues

--- hypercore/models/__init__.py
+++ hypercore/models/__init__.py
@@ -6,7 +6,7 @@ from .lorentz_resnet import Lorentz_ResNet
 from .lorentz_resnet import Lorentz_resnet18, Lorentz_resnet34, Lorentz_resnet50, Lorentz_resnet101, Lorentz_resnet152
 from .LViT import LViT
 from .tokenizer import Tokenizer
-from .graph_retriever import GRetriever
+# from .graph_retriever import GRetriever
 from .LCLIP import LCLIP
 from .Transformer_encoder import LTransformerEncoder
 from .lorentz_feedforward import LorentzFeedForward
