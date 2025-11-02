dc9e9fa0d79ed0410f45516723a43fdbd9faef29
white-richard <rich.white285@yahoo.com>
2025-11-02 11:58:42 -0800

    small changes

--- hypercore/optimizers/__init__.py
+++ hypercore/optimizers/__init__.py
@@ -3,3 +3,5 @@ from .radam import RiemannianAdam
 from .rsgd import RiemannianSGD
 from .initialize import Optimizer
 from .initialize import LR_Scheduler
+from .radamw import RiemannianAdamW
+from .radamw_new import RiemannianAdamW
